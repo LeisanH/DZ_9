7c79f09d4b39f68a020e6a7172b69773c5b5924c
Leisan <kapatyn@mail.ru>
2022-12-20 12:30:43 +0500

    Решение задачи 68

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-﻿/*   Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
- натуральных элементов в промежутке от M до N.
+﻿/*   Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+ Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29   */
 
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30   */
+using System.Timers;
 
-int m = 4, n = 8;
+int m = 3, n = 2;
 
-Console.Write($"{Summ(m,n)}");
+Console.Write($"{Accerman (m,n)}");
 
-int Summ (int left, int right)
+int Accerman (int m, int n)
 {
-    if (left >= right)
-        return left;
-    return left + Summ(left + 1, right);
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Accerman(m - 1, 1);
+    else
+        return Accerman(m - 1, Accerman(m, n - 1));
 }
 
